dfcc3877727c8554d585534b865d8e89b6f46098
thanh.do <jennytd0201@gmail.com>
2023-04-03 20:54:26 -0700

    hw9a

---
+++ ex2/accounting.js
@@ -0,0 +1,17 @@
+class Account {
+    constructor(name) {
+      this.name = name;
+      this.balance = 0;
+    }
+  
+    credit(amount) {
+      this.balance += amount;
+    }
+  
+    describe() {
+      return `Account owner: ${this.name}, balance: $${this.balance}`;
+    }
+  }
+  
+  module.exports = Account;
+  
